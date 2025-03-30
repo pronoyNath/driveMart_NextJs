6062a11b31cd62540e6de41407352ebd1d402586
Anthokhiya Nath <pronoynath890@gmail.com>
2025-03-30 14:22:04 +0600

    basic setup

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <div>
+      <h3 className="text-7xl">Hello World!</h3>
+      <Button variant={"link"}>Button</Button>
+    </div>
+  );
+}
